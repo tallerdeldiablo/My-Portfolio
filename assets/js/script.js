1c682aefde5acf9b1d547c4266ba6c7c64eff7df
unknown <rikardoan@gmail.com>
2021-10-21 23:24:22 -0400

    hamburgur click fixed js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-//yo
+var toggleButton  = document.getElementsByClassName('toggle')[0]
+var navbarLinks  = document.getElementsByClassName('navBar-list')[0]
+
+console.log(toggleButton)
+
+toggleButton.addEventListener('click', function() {
+    navbarLinks.classList.toggle('active')
+  })
